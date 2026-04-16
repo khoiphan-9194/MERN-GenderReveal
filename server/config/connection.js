0d34f244face95b73937b1fcaf2575173bf5aaff
Khoi Phan <jasonphan2631@gmail.com>
2026-04-15 20:28:47 -0700

    43

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/genderdb');
+mongoose.connect( 'mongodb://127.0.0.1:27017/genderdb'||process.env.MONGODB_URI);
 mongoose.connection.on("error", (err) => {
   console.error("MongoDB connection error:", err);
 });
